016aa740a49daf19f7cf4a599436cb8d704b4bf9
Sourcery AI <>
2023-03-02 06:30:45 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests, json, os
 
 class main():
-    def scrapeManga(url:str, saveToFile:bool=False):
-        id = url.split("/")
+    def scrapeManga(self, saveToFile:bool=False):
+        id = self.split("/")
 
         with open("cookies.json", "r") as file:
             cookie = json.loads(file.read())
@@ -30,38 +30,36 @@ class main():
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
         }
 
-        response = requests.get(url, cookies=cookies, headers=headers)
+        response = requests.get(self, cookies=cookies, headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
         tag = soup.find_all("span", {"class": "tags"})[2]
 
-        tags = []
-        pages = []
-
-        for i in tag.find_all("a"):
-            tags.append(i.find_all("span")[0].text)
-
-        for i in soup.find_all("a", {"class": "gallerythumb"}):
-            pages.append("https://nhentai.net/g" + i["href"])
-
+        tags = [i.find_all("span")[0].text for i in tag.find_all("a")]
+        pages = [
+            "https://nhentai.net/g" + i["href"]
+            for i in soup.find_all("a", {"class": "gallerythumb"})
+        ]
         jsonFormat = {
             "manga": {
                 "id": id[4],
-                "tags-info": {
-                    "count": int(len(tags)),
-                    "tags": tags
-                },
-                "artist": soup.find_all("span", {"class": "tags"})[3].find_all("span")[0].text,
-                "languages": soup.find_all("span", {"class": "tags"})[5].find_all("span")[0].text,
-                "categories": soup.find_all("span", {"class": "tags"})[6].find_all("span")[0].text,
-                "pages-info": {
-                    "count": int(len(pages)),
-                    "pages-link": pages
-                },
-                "uploaded": soup.find_all("span", {"class": "tags"})[8].find_all("time")[0]["datetime"]
+                "tags-info": {"count": len(tags), "tags": tags},
+                "artist": soup.find_all("span", {"class": "tags"})[3]
+                .find_all("span")[0]
+                .text,
+                "languages": soup.find_all("span", {"class": "tags"})[5]
+                .find_all("span")[0]
+                .text,
+                "categories": soup.find_all("span", {"class": "tags"})[6]
+                .find_all("span")[0]
+                .text,
+                "pages-info": {"count": len(pages), "pages-link": pages},
+                "uploaded": soup.find_all("span", {"class": "tags"})[8].find_all(
+                    "time"
+                )[0]["datetime"],
             }
         }
 
-        if saveToFile == False:
+        if not saveToFile:
             print("\n" + json.dumps(jsonFormat, indent=4) + "\n")
         else:
             with open(f"{id[4]}.json", "w") as file:
@@ -81,10 +79,9 @@ if __name__ == "__main__":
                     main.scrapeManga(manga)
                 else:
                     main.scrapeManga(manga, True)
+            elif saveResult == "n":
+                main.scrapeManga(f"https://nhentai.net/g/{manga}")
             else:
-                if saveResult == "n":
-                    main.scrapeManga("https://nhentai.net/g/" + manga)
-                else:
-                    main.scrapeManga("https://nhentai.net/g/" + manga, True)
+                main.scrapeManga(f"https://nhentai.net/g/{manga}", True)
         except Exception as err:
             print("\n" + "Error: " + str(err) + "\n")
